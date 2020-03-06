81d3c07157050fb226c9a3fed621a7ec010b6b69
luisantoniococa <luicocurd@gmail.com>
2020-03-05 18:51:21 -0600

    we finished the visualization and the filtering by date

--- steam_players.js
+++ steam_players.js
@@ -1,59 +1,214 @@
+// The entire code consist in 3 functions, one that runs all the other 2 and the 2 that change the options selected
 
-d3.json("top100_steam_player_data.json").then(function(data) {
-    console.log(data);
+
+//  this is the option changed one and is controlled by the dropdown in index.html
+// it takes the value of game 1 and then it creates a json call to gather the data, then it 
+// reads the dictionary and pushes the start and end dates of each game for further filtering
+// once filtered it compared the the start date of the selected game 1 with the ones in the dataset
+// based on the date it filters those that are more recent than the selected game 1 to avoid errors in the comparison.
+// it gets pushed to newgamenames and then it is used to build the selector 2 with the games 2.
+// finaly it updates the trace 1 with the plotly values
+function optionChanged1(game1){
+
+  // getting the data
+  d3.json("top100_steam_player_data.json").then(function(data) {
+
+    newgamenames = []
+    // going over the dictionray to find the keys, start month and end month
+    for (let [key, value] of Object.entries(data)) {
+      var monthdict = data[key].Month
+      var monthdictlength = Object.keys(monthdict).length
+      var monthdictend = data[key].Month[1]
+      var monthdictstart = data[key].Month[monthdictlength - 1]
+      var  gameNamesDict = {'key' : key, 'startmonth' : monthdictstart, 'endmonth': monthdictend}
+      // pushing changes to the dictionary so it can be built.
+      newgamenames.push(gameNamesDict)
+      // used methods to check that we were getting the rigth values
+              // console.log(`this is ${key} key and startmonth ${monthdict}`)
+              // console.log("this is going multiple times")
+      // another method to check we are in the rigth path
+
+              // if(game1 == key){
+              //   console.log(`this is ${game1}`)
+              //   // data.filter(x => x>)
+              //   // console.log(monthdict)
+
+              // }
+      
+      // we make sure to eliminate the last value of the dataset since it is refered to a non month datapoint
+
+      delete value["Month"][0]
+      delete value['Avg. Players'][0]
+      delete value['Gain'][0]
+      delete value['% Gain'][0]
+      delete value['Peak Players'][0]
+    }
+    // another method to check if we are in the rigth path when building the dictionary
+              // console.log(newgamenames)
+    // we use a smilar code earlier but this time we are getting the end month of game 1 to compare 
+    var monthdict = data[game1].Month
+    var monthdictlength = Object.keys(monthdict).length
+    var monthdictend = data[game1].Month[1]
+    var monthdictstart = data[game1].Month[monthdictlength - 1]
+    // make sure we are getting the right values
+              // console.log(monthdictstart)
+              // var date1 = Date.parse(monthdictstart)
+              // var date2 = Date.parse(monthdictend)
+              // if (date1 < date2){
+              //   console.log(`date 1 is less than date2`)
+              // }
+    // create a var that will hold the names of the filtered games by date
+    // we used the Date.parse() method to be able to compare the dates
+    var actualPrintNames = newgamenames.filter(x=> Date.parse(x.startmonth) > Date.parse(monthdictstart) )
+
+    // make sure we have the rigth values
+              // console.log(actualPrintNames)
+
+    // create the selector 2 that is going to be built based in the filtered var created before.
+
+    var selector2 = d3.select("#selGame2")
+    selector2.html("<option value='default'>--Game 2--</option>")
+    actualPrintNames.forEach(element=>{
+      selector2.append("option")
+      .property("value",element.key)
+      .text(element.key)
+
+    })
+    // update the trace to make sure it changes respectively with game 1 
+    var update = {
+    x: [Object.keys(data[game1]["Month"]).map(i => data[game1]["Month"][i]).reverse()],
+    y: [Object.keys(data[game1]["Avg. Players"]).map(i => data[game1]["Avg. Players"][i]).reverse()],
+    name: game1
+  };
+  // pushing the changes into the plot [0] is becuae we are changing the trace 1
+  Plotly.restyle('myDiv', update,[0]);
+
+  });
+}
+function optionChanged2(game2){
+  // making sure we get the rigth values
+                // console.log(game2)
+  d3.json("top100_steam_player_data.json").then(function(data) {
   
+  // geting read of the first datapoint since it is irrelevant and can mess the data
+    for (let [key, value] of Object.entries(data)) {
+      delete value["Month"][0]
+      delete value['Avg. Players'][0]
+      delete value['Gain'][0]
+      delete value['% Gain'][0]
+      delete value['Peak Players'][0]
+    }
+  // updating the trace 2 values with the selection game 2
+  var update = {
+    x: [Object.keys(data[game2]["Month"]).map(i => data[game2]["Month"][i]).reverse()],
+    y: [Object.keys(data[game2]["Avg. Players"]).map(i => data[game2]["Avg. Players"][i]).reverse()],
+    name: game2
+  };
   
 
+  // pushing the changes into the plot, [1] is becuae we are changing the trace 2
+  Plotly.restyle('myDiv', update, [1]);
+
+  });
+}
+
+
+// the init function builds the first plot with the first game in the list.
+function init(){
+  var selector1 = d3.select("#selGame1"); 
+  // an attempt of building the selector in this function
+            // var selector2 = d3.select("#selGame2");
+  d3.json('top100_steam_player_data.json').then((data)=> {
+    var gameNames = []
+    for (let [key, value] of Object.entries(data)) {
+  // similar to the function above after calling our data, we filter to find the names and the start and end date
+      var monthdict = data[key].Month
+      var monthdictlength = Object.keys(monthdict).length
+      var monthdictend = data[key].Month[1]
+      var monthdictstart = data[key].Month[monthdictlength - 1]
+      var  gameNamesDict = {'key' : key, 'startmonth' : monthdictstart, 'endmonth': monthdictend}
+      gameNames.push(gameNamesDict)
+      delete value["Month"][0]
+      delete value['Avg. Players'][0]
+      delete value['Gain'][0]
+      delete value['% Gain'][0]
+      delete value['Peak Players'][0]
+    }
+    // mkaing sure we get the rigth values
+                // console.log(keys)
+    gameNames.forEach(element => {
+    // making sure we get the rigth values
+                // console.log(element)
+      selector1.append("option")
+      .property("value",element.key)
+      .text(element.key)
+    // if we wanted to add a selector 2 in this function as the example above 
+                // selector2.append("option")
+                // .property("value",element.key)
+                // .text(element.key)
+
+
+    });
+
+    // creating the first plot with 2 traces, we also empty trace 2 to make sure it does not show unnesesary data
     var trace1 = {
-    
-        x: ['2017-01-04', '2017-01-05', '2017-01-06', '2017-01-09', '2017-01-10', '2017-01-11', '2017-01-12', '2017-01-13', '2017-01-17', '2017-01-18', '2017-01-19', '2017-01-20', '2017-01-23', '2017-01-24', '2017-01-25', '2017-01-26', '2017-01-27', '2017-01-30', '2017-01-31', '2017-02-01', '2017-02-02', '2017-02-03', '2017-02-06', '2017-02-07', '2017-02-08', '2017-02-09', '2017-02-10', '2017-02-13', '2017-02-14', '2017-02-15'], 
-        
-        close: [116.019997, 116.610001, 117.910004, 118.989998, 119.110001, 119.75, 119.25, 119.040001, 120, 119.989998, 119.779999, 120, 120.080002, 119.970001, 121.879997, 121.940002, 121.949997, 121.629997, 121.349998, 128.75, 128.529999, 129.080002, 130.289993, 131.529999, 132.039993, 132.419998, 132.119995, 133.289993, 135.020004, 135.509995], 
-        
-        decreasing: {line: {color: '#7F7F7F'}}, 
-        
-        high: [116.510002, 116.860001, 118.160004, 119.43, 119.379997, 119.93, 119.300003, 119.620003, 120.239998, 120.5, 120.089996, 120.449997, 120.809998, 120.099998, 122.099998, 122.440002, 122.349998, 121.629997, 121.389999, 130.490005, 129.389999, 129.190002, 130.5, 132.089996, 132.220001, 132.449997, 132.940002, 133.820007, 135.089996, 136.270004], 
-        
-        increasing: {line: {color: '#17BECF'}}, 
-        
-        line: {color: 'rgba(31,119,180,1)'}, 
-        
-        low: [115.75, 115.809998, 116.470001, 117.940002, 118.300003, 118.599998, 118.209999, 118.809998, 118.220001, 119.709999, 119.370003, 119.730003, 119.769997, 119.5, 120.279999, 121.599998, 121.599998, 120.660004, 120.620003, 127.010002, 127.779999, 128.160004, 128.899994, 130.449997, 131.220001, 131.119995, 132.050003, 132.75, 133.25, 134.619995], 
-        
-        open: [115.849998, 115.919998, 116.779999, 117.949997, 118.769997, 118.739998, 118.900002, 119.110001, 118.339996, 120, 119.400002, 120.449997, 120, 119.550003, 120.419998, 121.669998, 122.139999, 120.93, 121.150002, 127.029999, 127.980003, 128.309998, 129.130005, 130.539993, 131.350006, 131.649994, 132.460007, 133.080002, 133.470001, 135.520004], 
-        
-        type: 'candlestick', 
-        xaxis: 'x', 
-        yaxis: 'y'
-    };
-    
-    var data = [trace1];
-    
-    var layout = {
-        dragmode: 'zoom', 
-        margin: {
-        r: 10, 
-        t: 25, 
-        b: 40, 
-        l: 60
-        }, 
-        showlegend: false, 
-        xaxis: {
-        autorange: true, 
-        domain: [0, 1], 
-        range: ['2017-01-03 12:00', '2017-02-15 12:00'], 
-        rangeslider: {range: ['2017-01-03 12:00', '2017-02-15 12:00']}, 
-        title: 'Date', 
-        type: 'date'
-        }, 
-        yaxis: {
-        autorange: true, 
-        domain: [0, 1], 
-        range: [114.609999778, 137.410004222], 
-        type: 'linear'
-        }
-    };
-    
-    Plotly.newPlot('myDiv', data, layout);
+        x: Object.keys(data["Counter-Strike: Global Offensive"]["Month"]).map(i => data["Counter-Strike: Global Offensive"]["Month"][i]).reverse(),
+        y: Object.keys(data["Counter-Strike: Global Offensive"]["Avg. Players"]).map(i => data["Counter-Strike: Global Offensive"]["Avg. Players"][i]).reverse(),
+        name: "Counter-Strike: Global Offensive",
+        type: 'scatter'
+      };
+      
+      var trace2 = {
+        // x: Object.keys(data["Dota 2"]["Month"]).map(i => data["Dota 2"]["Month"][i]).reverse(),
+        // y: Object.keys(data["Dota 2"]["Avg. Players"]).map(i => data["Dota 2"]["Avg. Players"][i]).reverse(),
+        // name: "Dota 2",
+        x:  [],
+        y: [],
+        name: "",
+        type: 'scatter'
 
-});
+      };
+      var layout = {
+        title: "Average Players per month ",
+        xaxis: {
+          title: 'Month',
+          tickformat: '%B %Y',
+          autorange: true,
+          titlefont: {
+            family: 'Arial, sans-serif',
+            size: 18,
+            color: 'lightgrey'
+          },
+          showticklabels: true,
+          tickangle: 'auto',
+           //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
+          tickfont: {
+            family: 'Old Standard TT, serif',
+            size: 8,
+            color: 'black'
+          },
+        },
+        yaxis: {
+          title: 'AVG. Players per month',
+          titlefont: {
+            family: 'Arial, sans-serif',
+            size: 18,
+            color: 'lightgrey'
+          },
+          showticklabels: true,
+          tickangle: 45,
+          tickfont: {
+            family: 'Old Standard TT, serif',
+            color: 'black'
+          },
+        }
+      };
+      
+      var dataPlot = [trace1, trace2];
+      // var dataPlot = [trace2]
+      
+      Plotly.newPlot('myDiv', dataPlot, layout);
+  })
+}
+// calling the function to run the script
+init();
